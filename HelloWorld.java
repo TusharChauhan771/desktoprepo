87ee4de588dece2e2d4431a3c636bd4aac9d61e6
TusharChauhan771 <128404765+TusharChauhan771@users.noreply.github.com>
2023-04-08 12:48:49 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
-public class HelloWorld {
+import java.util.Scanner;
+
+public class PersonalizedGreeting {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "! Welcome to the world of Java programming.");
     }
 }
